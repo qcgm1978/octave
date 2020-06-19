e14f3fd750c6536bdfc6c6789404576279e8ca10
Philipp <zhanghongliang@192.168.1.100>
2020-06-20 02:35:34 +0800

    det

--- science/lottery.m
+++ science/lottery.m
@@ -1,2 +1,2 @@
-clc clear all#pkg load financialfunction prop=getLotteryProp(year)  thisYear=clock()(1)  differ=thisYear-year  if(differ<=3)    endif
-endfunctiongetLotteryProp(2011)
+clc clear all#pkg load financialfunction prop=getNextLotteryProp(year)  thisYear=clock()(1)  differ=year-2011  if(differ>=3)  prop=12*3+(differ-3)*6+1  endif
+endfunctiongetNextLotteryProp(2020)
