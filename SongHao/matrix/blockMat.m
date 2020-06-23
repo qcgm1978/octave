b83cd591d9652c829c158c4fc51e28a782b0ebcb
Philipp <zhanghongliang@192.168.1.105>
2020-06-24 01:07:51 +0800

    ditto

--- SongHao/matrix/blockMat.m
+++ SongHao/matrix/blockMat.m
@@ -1,7 +1,11 @@
 clc
 clear all
 A=[1 1 3 4 0;2 0 1 1 0;1 1 1 1 3; 4 1 1 1 0];
-function normMat=getNormMat(oneCount,zeroCount=0,m=oneCount+zeroCount,n=m)
+function normMat=getNormMat(oneCount,zeroCount=0,n=oneCount+zeroCount)
+  m=oneCount+zeroCount;
+  if(n<m)
+    error('columns less than total of oneCount and zeroCount')
+  endif
   normMat=zeros(m,n);
   for i=1:oneCount
     normMat(i,i)=1;
@@ -11,13 +15,12 @@ assert1(find(getNormMat(2,5)),[1;9])
 normMat=getNormMat(2,5);
 normMat0=getNormMat(2);
 normMat1=getNormMat(2,5,8);
-normMat2=getNormMat(2,5,8,9);
-normMat3=getNormMat(2,5,8,6);
+normMat2=getNormMat(2,5,8);
+fail('normMat3=getNormMat(2,5,6);','columns less than total of oneCount and zeroCount')
+assert(normMat4=getNormMat(0,5,6),zeros(5,6))
 assert1(normMat0,eye(2))
 assert1(find(normMat),[1;9])
-assert1(find(normMat1),[1;10])
-assert1(find(normMat2),[1;10])
-assert1(find(normMat3),[1;10])
-assert1(size(normMat2),[8 9])
-assert1(size(normMat3),[8 6])
+assert1(find(normMat1),[1;9])
+assert1(find(normMat2),[1;9])
+assert1(size(normMat2),[7 8])
 assert1(size(normMat),[7,7])
