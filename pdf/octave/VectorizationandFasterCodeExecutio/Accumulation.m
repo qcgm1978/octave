d296fa31fd289112ad66f4cbf0bcb8168c4d4fc9
Philipp <zhanghongliang@192.168.1.107>
2020-07-05 00:27:48 +0800

    ditto

--- pdf/octave/VectorizationandFasterCodeExecutio/Accumulation.m
+++ pdf/octave/VectorizationandFasterCodeExecutio/Accumulation.m
@@ -4,7 +4,7 @@ u=[1 2 3];
 index=[1 2 3 1 2 3 3 2 1];
 assert1(u(index),index)
 x = [91, 92, 90, 92, 90, 89, 91, 89, 90, 100, 100, 100];
- [u, i, j] = unique (x);
+[u, i, j] = unique (x);
 assert1( u(j),x)
 m=j;
 m(1,[1,2])=m(1,[2,1]);
@@ -24,15 +24,34 @@ indices = [group ones(size(group))];
 sums = accumarray(indices, data);
 sums1 = accumarray(indices, data, [numel(unique(group)) 1], @sum);
 assert1(sums,sums1,getAccumarray(indices, data))
+maxData = accumarray(indices, data, [numel(unique(group)) 1], @max);
+maxData1 = getAccumarray(indices, data, [numel(unique(group)) 1], @max);
+assert1(maxData,maxData1)
+maxData = accumarray(indices, data, [numel(unique(group)) 1], ...
+@(x)~any(isfinite(x)));
+maxData1 = getAccumarray(indices, data, [numel(unique(group)) 1], ...
+@(x)~any(isfinite(x)));
+assert1(maxData,maxData1)
+data(end) = Inf;
+maxData = accumarray(indices, data, [numel(unique(group)) 1], ...
+@(x)~any(isfinite(x)));
+maxData1 = getAccumarray(indices, data, [numel(unique(group)) 1], ...
+@(x)~any(isfinite(x)));
+assert1(maxData,maxData1)
+maxData = accumarray(indices, data, [numel(unique(group)) 1], ...
+@(x)all(isfinite(x)));
+maxData1 = getAccumarray(indices, data, [numel(unique(group)) 1], ...
+@(x)all(isfinite(x)));
+assert1(maxData,maxData1)
 val = 101:106;
 subs = [1 1; 2 2; 3 2; 1 1; 2 2; 4 1];
 A = accumarray(subs,1);
 B=getAccumarray(subs,1);
 assert1(A,B)
 subs=[1, 1, 1;
-                      2, 1, 2;
-                      2, 3, 2;
-                      2, 1, 2;
+2, 1, 2;
+2, 3, 2;
+2, 1, 2;
 2, 3, 2];
 val=101:105;
 assert1(accumarray (subs, val),getAccumarray (subs, val))
@@ -77,6 +96,7 @@ A1 = accumarray(subs,val,[],[],NaN);
 A2 = getAccumarray(subs,val,[],[],NaN);
 assert1(A1,A2)
 val = [34 22 19 85 53 77 99 6];
-subs = [1 1; 400 400; 80 80; 1 1; 400 400; 400 400; 80 80; 1 1]
-A1 = accumarray(subs,val,[],[],[],true)
-#A2 = getAccumarray(subs,val,[],[],[],true)
+subs = [1 1; 400 400; 80 80; 1 1; 400 400; 400 400; 80 80; 1 1];
+#A1 = accumarray(subs,val,[],[],[],true);
+#A2 = getAccumarray(subs,val,[],[],[],true);#performance optimization
+assert1(A1,A2)
