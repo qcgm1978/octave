85f5b89013b3d0104edf12f28931fd4eb88bc45f
Philipp <zhanghongliang@192.168.1.102>
2020-07-08 01:19:02 +0800

    Vectorization and Faster Code Execution

--- pdf/octave/VectorizationandFasterCodeExecutio/Examples.m
+++ pdf/octave/VectorizationandFasterCodeExecutio/Examples.m
@@ -1,14 +1,12 @@
 clc
 clear all
-A=rand(randi([10,100]));
-n = length (A);
-B = zeros (n, 2);
-for i = 1:rows (A)-1
-  ## this will be two columns, the first is the difference and ## the second the mean of the two elements used for the diff. 
-  if(i==rows(A))
-  i
-  A(i+1)
-  endif
+A=randi(5,1,6);
+n = length (A)-1;
+B = zeros (n, 2);#set the size of the result matrix first, then insert values into it
+for i = 1:length (A)-1
+  ## this will be two columns, the first is the difference and the second the mean of the two elements used for the diff. 
   B(i,:) = [A(i+1)-A(i), (A(i+1) + A(i))/2];
 endfor
-C = [diff(A)(:), 0.5*(A(1:end-1)+A(2:end))(:)];
+#C = diff(A)';
+#D=.5*(A(1:end-1)+A(2:end))';#whole-array operations
+assert1(B,[diff(A)',.5*(A(1:end-1)+A(2:end))'])#Avoid computing costly intermediate results
